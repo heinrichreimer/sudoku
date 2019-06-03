622af36490a146e2eb2f7d7c5a18631baa5be3a5
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2019-06-04 01:59:53 +0200

    Add sudoku classes

---
+++ lib/sudoku.dart
@@ -0,0 +1,38 @@
+class Sudoku {
+}
+
+enum Digit {
+  ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE
+}
+
+class Row {
+
+}
+
+class Column {
+
+}
+
+class Block {
+
+}
+
+class Session {
+
+}
+
+class Solver {
+
+}
+
+enum Difficulty {
+  NOOB, EASY, MEDIUM, HARD, EXTREME
+}
+
+abstract class Source {
+
+}
+
+class Generator implements Source {
+
+}

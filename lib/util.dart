03cd348ddff979e8db17aa5fe92c58abb37d7920
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2019-06-04 17:57:18 +0200

    Reformat code

--- lib/util.dart
+++ lib/util.dart
@@ -1,6 +1,4 @@
-Iterable<T> distinct<T>(Iterable<T> collection) =>
-    Set()
-      ..addAll(collection);
+Iterable<T> distinct<T>(Iterable<T> collection) => Set()..addAll(collection);
 
 bool isDistinct<T>(Iterable<T> collection) =>
     collection.length == distinct(collection).length;

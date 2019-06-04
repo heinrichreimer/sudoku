31892d3d1bb59de3402220fad6c0e8392d064dab
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2019-06-04 17:00:21 +0200

    Add collection utils

---
+++ lib/util.dart
@@ -0,0 +1,6 @@
+Iterable<T> distinct <T>(Iterable<T> collection) =>
+    Set()
+      ..addAll(collection);
+
+bool isDistinct <T>(Iterable<T> collection) =>
+    collection.length == distinct(collection).length;

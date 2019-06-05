ea6de2b8ae63b15d5e68af7ecb90bcda52d9198b
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2019-06-05 16:24:15 +0200

    Change solver signature

--- lib/sudoku/solver.dart
+++ lib/sudoku/solver.dart
@@ -1,9 +1,5 @@
-import 'package:meta/meta.dart';
 import 'package:sudoku/sudoku/sudoku.dart';
 
-@immutable
-class Solver {
-  Sudoku solve(Sudoku sudoku) {
-    throw UnimplementedError();
-  }
+abstract class Solver {
+  Sudoku solve(Sudoku sudoku);
 }

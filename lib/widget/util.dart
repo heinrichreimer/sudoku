137b713578ed6c0040237ff419eb94407c8d002c
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2019-06-05 23:53:32 +0200

    Fix square grid rendering

--- lib/widget/util.dart
+++ lib/widget/util.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 @immutable
@@ -6,6 +7,26 @@ class Square extends AspectRatio {
     Key key,
     Widget child,
   }) : super(key: key, aspectRatio: 1, child: child);
+}
+
+@immutable
+class DividerBox extends StatelessWidget {
+  final Axis axis;
+  final BorderSide border;
+
+  const DividerBox({this.border, this.axis});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: axis == Axis.horizontal ? border : BorderSide.none,
+          left: axis == Axis.vertical ? border : BorderSide.none,
+        ),
+      ),
+    );
+  }
 }
 
 @immutable
@@ -18,37 +39,42 @@ class SquareGrid extends StatelessWidget {
     Key key,
     @required this.size,
     @required this.generator,
-    this.divider,
+    this.divider: BorderSide.none,
   }) : super(key: key);
+
+  Widget buildRow(BuildContext context, int row) {
+    return Row(
+        children: List.generate(2 * size - 1, (column) {
+          if (column.isOdd) {
+            return DividerBox(
+              border: divider,
+              axis: Axis.vertical,
+            );
+          } else {
+            return Flexible(
+              child: Square(
+                child: generator(row ~/ 2, column ~/ 2),
+              ),
+            );
+          }
+        }));
+  }
 
   @override
   Widget build(BuildContext context) {
-    BorderSide _calculateBorderSide(int position) {
-      if (position + 1 == size) {
-        return BorderSide.none;
-      } else {
-        return divider ?? BorderSide.none;
-      }
-    }
-
     return Square(
-      child: GridView.count(
-        crossAxisCount: size,
-        physics: NeverScrollableScrollPhysics(),
-        children: List.generate(size * size, (index) {
-          int row = index ~/ size;
-          int column = index % size;
-          return Container(
-            child: generator(row, column),
-            decoration: BoxDecoration(
-              border: Border(
-                left: _calculateBorderSide(size - 1 - column),
-                right: _calculateBorderSide(column),
-                top: _calculateBorderSide(size - 1 - row),
-                bottom: _calculateBorderSide(row),
-              ),
-            ),
-          );
+      child: Column(
+        children: List.generate(2 * size - 1, (row) {
+          if (row.isOdd) {
+            return DividerBox(
+              border: divider,
+              axis: Axis.horizontal,
+            );
+          } else {
+            return Flexible(
+              child: buildRow(context, row),
+            );
+          }
         }),
       ),
     );
